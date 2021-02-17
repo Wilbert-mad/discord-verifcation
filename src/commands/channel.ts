6971fdf2a5b19c799d12033d25e96331b6c5c83d
Wilbert-mad <angelbecerrah548@gmail.com>
2021-02-16 20:15:17 -0600

    feat(channel cmd): set sub commands move this from setChannel to a channe commands/subcommand for channels like welcome channel, and other configs comming

--- src/commands/channel.ts
+++ src/commands/channel.ts
@@ -1,21 +1,38 @@
-import type { Message } from 'discord.js';
+import { Message, MessageEmbed } from 'discord.js';
+import type { dataCache } from '../events/message';
 import BaseCommand from '../structures/BaseCommand';
 import type verifyClient from '../structures/VerifyClient';
 
 export default class SetChannel extends BaseCommand {
   constructor() {
-    super('setchannel', {
-      aliases: ['sch'],
+    super('channel', {
+      aliases: ['ch'],
+      sunCommands: true,
     });
   }
 
-  async run(client: verifyClient, message: Message, [IDch]: string[]) {
+  async run(client: verifyClient, message: Message, [cmd, ...args]: string[], data: dataCache) {
     if (!message.guild) return;
-    const channel = message.mentions.channels.first() || message.guild?.channels.cache.get(IDch);
-    if (!channel) return message.channel.send('❌ | No channel found!');
-    await client.databaseManiger
-      .update('ChannelId', channel.id, message.guild.id)
-      .then(() => message.channel.send(`Channel set to ${channel.toString()}`))
-      .catch(e => message.channel.send(`Error updating channel: ${e.message || e}`));
+    const helpCommand = new MessageEmbed().setAuthor('Channel sub Commands')
+      .setDescription(`Avalable sub commands:\n \`welcome\` Sets the welcome channel.
+      e.g: \`${data.prefix}channel welcome #welcome-channel\`
+      `);
+
+    switch (cmd) {
+      case 'welcome': {
+        const channel = message.mentions.channels.first() || message.guild.channels.cache.get(args[0]);
+        if (!channel) return message.channel.send('❌ | Channel not found!');
+        await client.databaseManiger
+          .update('ChannelId', channel.id, message.guild.id)
+          .then(() => message.channel.send(`Channel set to ${channel.toString()}`))
+          .catch(e => message.channel.send(`Error updating channel: ${e.message || e}`));
+        break;
+      }
+
+      default: {
+        message.channel.send(helpCommand);
+        break;
+      }
+    }
   }
 }
