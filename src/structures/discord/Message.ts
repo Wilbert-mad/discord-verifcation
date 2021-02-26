680a72fb8f6323c79f4bfe510f23396bce4348a7
Wilbert-mad <angelbecerrah548@gmail.com>
2021-02-25 18:54:46 -0600

    feat(Message): translate method spite translate from the send method

--- src/structures/discord/Message.ts
+++ src/structures/discord/Message.ts
@@ -3,10 +3,15 @@ import type verifyClient from '../VerifyClient';
 import type { verifyGuild } from './Guild';
 
 export class verifyMessage extends Message {
-  async send(key: string) {
+  async translate(key: string) {
     const data = await (this.guild as verifyGuild).data();
     const languages = (this.client as verifyClient).languages.get(data?.Language ?? 'en-us');
     const message = languages?.get(key);
+    return message;
+  }
+
+  async send(key: string) {
+    const message = await this.translate(key);
     if (typeof message == 'string') return this.channel.send(message);
     // if (typeof message == 'function') return this.channel.send(message());
     else return this.channel.send('TRANSLATION_NOTFOUND');
