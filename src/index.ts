60d4af74a9a693bd66cdec5ecb7f9466e0730c9d
Wilbert-mad <angelbecerrah548@gmail.com>
2021-03-06 19:27:06 -0600

    production: yaml#12 fix

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import './structures/discord';
 import verifyClient from './structures/VerifyClient';
 const client = new verifyClient();
 
-// Production time
 (async () => {
   await client.start(TOKEN, KEY);
 })();
